54a5baaef2cfe9b55fd63b3bfbbf88dfa6065a41
shockwave07 <varunraskar22@gmail.com>
2023-12-22 17:46:32 +0530

    final flask app

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,24 @@
 # app/__init__.py
 from flask import Flask
+from detectron2.data import DatasetCatalog, MetadataCatalog
+from .routes import main_bp
+from .filters import b64encode
+from .utils import get_sign_dicts
 
 def create_app():
-    
     app = Flask(__name__)
-    from app.filters import b64encode
+
+    # Register your custom dataset during application initialization
+    for d in ["train", "val"]:
+        DatasetCatalog.register("traffic_sign_" + d, lambda d=d: get_sign_dicts('./' + d))
+        MetadataCatalog.get("traffic_sign_" + d).set(thing_classes=['prohibitory', 'danger', 'mandatory', 'other'])
+
+    # Register custom filters
     app.jinja_env.filters['b64encode'] = b64encode
 
-    from app.routes import main_bp
+    # Register blueprints
     app.register_blueprint(main_bp)
 
     return app
+
 
--- app/utils.py
+++ app/utils.py
@@ -9,27 +9,71 @@ from detectron2.utils.visualizer import Visualizer
 from detectron2 import model_zoo
 from flask import current_app
 import json
-from detectron2.data import MetadataCatalog
+from detectron2.data import DatasetCatalog, MetadataCatalog
 
 from PIL import Image
+import os
+import numpy as np
+import json
+from detectron2.structures import BoxMode
+
+
+def get_sign_dicts(directory):
+    classes = ['prohibitory', 'danger', 'mandatory', 'other']
+    dataset_dicts = []
+    img_id = 0
+    for filename in [file for file in os.listdir(directory) if file.endswith('.json')]:
+        json_file = os.path.join(directory, filename)
+        with open(json_file) as f:
+            img_anns = json.load(f)
+
+        record = {}
+
+        filename = os.path.join(directory, img_anns["imagePath"])
+
+        record["file_name"] = filename
+        record["image_id"] = img_id
+        record["height"] = img_anns["imageHeight"]
+        record["width"] = img_anns["imageWidth"]
+
+        annos = img_anns["shapes"]
+        objs = []
+        for anno in annos:
+            px = [a[0] for a in anno['points']]
+            py = [a[1] for a in anno['points']]
+            poly = [(x, y) for x, y in zip(px, py)]
+            poly = [p for x in poly for p in x]
+
+            obj = {
+                "bbox": [np.min(px), np.min(py), np.max(px), np.max(py)],
+                "bbox_mode": BoxMode.XYXY_ABS,
+                "segmentation": [poly],
+                "category_id": classes.index(anno['label']),
+                "iscrowd": 0
+            }
+            objs.append(obj)
+        record["annotations"] = objs
+        dataset_dicts.append(record)
+        img_id += 1
+    return dataset_dicts
+
+
 
 def perform_instance_segmentation(image, output_format='png'):
     cfg = get_cfg()
 
-    cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
+    cfg.merge_from_file("config.yml")
     cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
     cfg.MODEL.DEVICE = "cpu"
-    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
+    cfg.MODEL.WEIGHTS = os.path.join("models","model_final.pth")
 
     predictor = DefaultPredictor(cfg)
 
     img = cv2.imdecode(np.fromstring(image.read(), np.uint8), cv2.IMREAD_UNCHANGED)
-
     outputs = predictor(img[..., ::-1])
+    
+    v = Visualizer(img[:, :, ::-1], MetadataCatalog.get("traffic_sign_train"), scale=0.5)
 
-    MetadataCatalog.get(cfg.DATASETS.TRAIN[0])
-
-    v = Visualizer(img[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2)
     result_image = v.draw_instance_predictions(outputs["instances"].to("cpu")).get_image()
 
     # Convert the VisImage to a NumPy array
