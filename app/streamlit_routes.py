1733cf6539d34b94a67a31f9dcf4e8ad11cfe335
vikram-307 <83663958+vikram-307@users.noreply.github.com>
2023-12-22 13:10:13 +0530

    Added changes to streamlitUI

--- app/streamlit_routes.py
+++ app/streamlit_routes.py
@@ -1,23 +1,15 @@
-from flask import Blueprint, render_template, request, send_from_directory
+from flask import Blueprint, request, send_from_directory, send_file
 from app.utils import perform_instance_segmentation
-import base64
 
 main_bp = Blueprint('main', __name__)
-
-@main_bp.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        # Handle image upload and perform instance segmentation
-        result_image = perform_instance_segmentation(request.files['image'], output_format='png')
-
-        return render_template('index.html', result_image=result_image)
-
-    return render_template('index.html')
 
 @main_bp.route('/segment', methods=['POST'])
 def segment():
     if 'image' in request.files:
+        # Handle image upload and perform instance segmentation
         result_image = perform_instance_segmentation(request.files['image'], output_format='png')
+
+        # Return image bytes
         return result_image
     else:
         return "No image received"
